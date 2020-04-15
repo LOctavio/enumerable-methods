b553804f6270820b9888f76ebe9aab33413a8147
LOctavio <luis_ram1997@hotmail.com>
2020-04-15 14:51:43 -0500

    add my_each_with_index method

--- enumerable.rb
+++ enumerable.rb
@@ -6,7 +6,20 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield self[i], i
+      i+=1
+    end
+  end
 end
 
-puts "my_each method:"
+puts 'my_each method:'
 [1, 2, 3, 4].my_each { |x| puts x }
+
+puts 'my_each_with_index method:'
+hash = {}
+%w[first second third fourth].my_each_with_index { |item, index| hash[item] = index }
+puts hash
