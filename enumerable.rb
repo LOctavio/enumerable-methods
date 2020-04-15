2c36ee4d56729b5e4c8a2430039d9e43cba2867d
LOctavio <luis_ram1997@hotmail.com>
2020-04-15 16:26:22 -0500

    add my_any? method

--- enumerable.rb
+++ enumerable.rb
@@ -34,19 +34,38 @@ module Enumerable
     end
     condition
   end
+
+  def my_any?
+    i = 0
+    condition = false
+    while i < length
+      condition = true if yield self[i]
+      i += 1
+    end
+    condition
+  end
 end
 
 puts 'my_each method:'
 [1, 2, 3, 4].my_each { |x| puts x }
 puts
+
 puts 'my_each_with_index method:'
 hash = {}
 %w[first second third fourth].my_each_with_index { |item, index| hash[item] = index }
 puts hash
 puts
+
 puts 'my_select_method:'
 puts([1, 2, 3, 4, 5, 6, 7, 8, 9].my_select { |x| (x % 3).zero? })
 puts
-puts 'my_all? method'
+
+puts 'my_all? method:'
 puts([8, 4, 3, 9, 5].my_all? { |x| x >= 3 })
 puts([8, 4, 3, 9, 5].my_all? { |x| x >= 4 })
+puts
+
+puts 'my_any? method:'
+puts([8, 4, 3, 9, 5].my_any? { |x| x >= 3 })
+puts([8, 4, 3, 9, 5].my_any? { |x| x >= 10 })
+puts
