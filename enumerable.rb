f76871ff26b21d3469dcb77c1a5fe180cc1b90e0
LOctavio <luis_ram1997@hotmail.com>
2020-04-16 10:16:59 -0500

    add condition to run both with and without block given to first three methods

--- enumerable.rb
+++ enumerable.rb
@@ -1,28 +1,40 @@
 module Enumerable
   def my_each
-    i = 0
-    while i < length
-      yield self[i]
-      i += 1
+    if block_given?
+      i = 0
+      while i < length
+        yield self[i]
+        i += 1
+      end
+    else
+      to_enum
     end
   end
 
   def my_each_with_index
-    i = 0
-    while i < length
-      yield self[i], i
-      i += 1
+    if block_given?
+      i = 0
+      while i < length
+        yield self[i], i
+        i += 1
+      end
+    else
+      self
     end
   end
 
   def my_select
-    i = 0
-    arr = []
-    while i < length
-      arr << self[i] if yield self[i]
-      i += 1
+    if block_given?
+      i = 0
+      arr = []
+      while i < length
+        arr << self[i] if yield self[i]
+        i += 1
+      end
+      arr
+    else
+      to_enum
     end
-    arr
   end
 
   def my_all?
@@ -58,16 +70,21 @@ end
 
 puts 'my_each method:'
 [1, 2, 3, 4].my_each { |x| puts x }
+puts [1, 2, 3, 4].my_each
 puts
 
 puts 'my_each_with_index method:'
 hash = {}
 %w[first second third fourth].my_each_with_index { |item, index| hash[item] = index }
 puts hash
+hash2 = {}
+%w[first second third fourth].my_each_with_index
+puts hash2
 puts
 
 puts 'my_select_method:'
 puts([1, 2, 3, 4, 5, 6, 7, 8, 9].my_select { |x| (x % 3).zero? })
+puts [1, 2, 3, 4, 5, 6, 7, 8, 9].my_select
 puts
 
 puts 'my_all? method:'
