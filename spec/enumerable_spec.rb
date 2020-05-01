1614e5f85c8a65fa384fc2a7eb768c400172f1e3
Luis Octavio <luis_ram1997@hotmail.com>
2020-05-01 12:21:14 -0500

    add test for my_all? method

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -21,4 +21,12 @@ describe Enumerable do
       expect([1, 2, 3, 4, 5, 6, 7, 8, 9].my_select { |x| (x % 2).zero? }).to eql([2, 4, 6, 8])
     end
   end
+  describe '#my_all?' do
+    it 'all the values are bigger or equal than 3' do
+      expect([8, 4, 3, 9, 5].my_all? { |x| x >= 3 }).to eql(true)
+    end
+    it 'all the values are not bigger or equal than 4' do
+      expect([8, 4, 3, 9, 5].my_all? { |x| x >= 4 }).to eql(false)
+    end
+  end
 end
