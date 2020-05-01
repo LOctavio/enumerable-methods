6e01efab99c92924695e7dc9e7c1f14fd1cb157a
Henzi <mugunga5@gmail.com>
2020-05-01 18:55:20 +0200

    add testing for my_each_index_with

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -3,7 +3,14 @@ require './enumerable.rb'
 describe Enumerable do
   describe '#my_each' do
     it 'return an array' do
-      expect([1,2,3,4].each { |x| print x }).to eql([1, 2, 3, 4])
+      expect([1,2,3,4].my_each { |x| print x }).to eql([1, 2, 3, 4])
+    end
+  end
+  describe "#my_each_with_index" do
+    it "" do
+      hash = Hash.new
+      [1, 2, 3, 4].my_each_with_index { |val,index| hash[val]=index }
+      expect( hash ).to eql({1 => 0, 2=>1, 3 => 2, 4 => 3})
     end
   end
 end
